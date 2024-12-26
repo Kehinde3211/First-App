0cc381d1bc54422447f2ccdad9be66c7215bce03
kehinde3211 <kehindeolayinka039@gmail.com>
2024-12-26 10:32:59 +0100

    initial commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,114 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+ <View>
+            <Text style={styles.An}>A for Apple</Text>
+            <Text style={styles.Bn}>B for Ball</Text>
+            <Text style={styles.Cn}>C for Cat</Text>
+            <Text style={styles.Dn}>D for Dog</Text>
+            <Text style={styles.En}>E for Egg</Text>
+            <Text style={styles.Fn}>F for Fish</Text>
+            <Text style={styles.Gn}>G for Goat</Text>
+            <Text style={styles.Hn}>H for House</Text>
+            <Text style={styles.In}>I for Ink</Text>
+            <Text style={styles.Jn}>J for Jug</Text>
+            <Text style={styles.Kn}>K for Key</Text>
+            <Text style={styles.Ln}>L for Lion</Text>
+            <Text style={styles.Mn}>M for Mango</Text>
+            <Text style={styles.Nn}>N for Nurse</Text>
+            <Text style={styles.On}>O for Orange</Text>
+            <Text style={styles.Pn}>P for Pot</Text>
+            <Text style={styles.Qn}>Q for Queen</Text>
+            <Text style={styles.Rn}>R for Rat</Text>
+            <Text style={styles.Sn}>S for Snake</Text>
+            <Text style={styles.Tn}>T for Teeth</Text>
+            <Text style={styles.Un}>U for Uniform</Text>
+            <Text style={styles.Vn}>V for Van</Text>
+            <Text style={styles.Wn}>W for Watch</Text>
+            <Text style={styles.Xn}>X for Xyl ophone</Text>
+            <Text style={styles.Yn}>Y for Yam</Text>
+            <Text style={styles.Zn}>Z for Zion</Text>
+    </View>
+        )
+  };
+export default App;
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const styles = StyleSheet.create({
+An: {
+            backgroundColor: 'white',
+  },
+Bn: {
+            backgroundColor: 'navy-blue',
+  },
+Cn: {
+            backgroundColor: 'tomato',
+  },
+Dn: {
+            backgroundColor: 'cyan',
+  },
+En: {
+            backgroundColor: 'lime',
+  },
+Fn: {
+            backgroundColor: 'maroon',
+  },
+Gn: {
+            backgroundColor: 'orange',
+  },
+Hn: {
+            backgroundColor: 'olive',
+  },
+In: {
+            backgroundColor: 'tan',
+  },
+Jn: {
+            backgroundColor: 'teal',
+  },
+Kn: {
+            backgroundColor: 'blue',
+  },
+Ln: {
+            backgroundColor: 'emerald',
+  },
+Mn: {
+            backgroundColor: 'green',
+  },
+Nn: {
+            backgroundColor: 'gold',
+  },
+On: {
+            backgroundColor: 'purple',
+  },
+Pn: {
+            backgroundColor: 'cream',
+  },
+Qn: {
+            backgroundColor: 'indigo',
+  },
+Rn: {
+            backgroundColor: 'violet',
+  },
+Sn: {
+            backgroundColor: 'pink',
+  },
+Tn: {
+            backgroundColor: 'orange',
+  },
+Un: {
+            backgroundColor: 'yellow',
+  },
+Vn: {
+            backgroundColor: 'blue',
+  },
+Wn: {
+            backgroundColor: 'green',
+  },
+Xn: {
+            backgroundColor: 'grey',
+  },
+Yn: {
+            backgroundColor: 'blue',
+  },
+Zn: {
+            backgroundColor: 'ash',
+  },
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+})
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
